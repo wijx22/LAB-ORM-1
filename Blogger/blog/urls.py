4f53d7d9b2c891cfa1c91042b36c8d7244cb25a1
wijdan <wjdanalbdalzyz@gmail.com>
2025-03-08 22:36:36 +0300

    OrM Ubdate and Delete and search

--- Blogger/blog/urls.py
+++ Blogger/blog/urls.py
@@ -5,9 +5,10 @@ urlpatterns = [
     path('', home, name='home'),
     path('add/', add_post, name='add_post'),
     path('post/<int:post_id>/', post_detail, name='post_detail'),
+    path('post/<int:post_id>/delete/', delete_post, name='delete_post'),
     path('post/<int:post_id>/edit/', update_post, name='update_post'),
-    path('post/<int:post_id>/delete/', delete_post, name='delete_post'),
-    path('post/<int:post_id>/delete/', delete_post, name='delete_post'),
+
+ 
 
 
 
--- Blogger/blog/views.py
+++ Blogger/blog/views.py
@@ -52,3 +52,39 @@ def delete_post(request, post_id):
         post.delete()
         return redirect('home')
     return render(request, 'blog/delete_post.html', {'post': post})
+
+
+
+
+
+
+def home(request):
+    query = request.GET.get('q')  
+    if query:
+        posts = Post.objects.filter(title__icontains=query) 
+    else:
+        posts = Post.objects.all().order_by('-published_at')  
+    return render(request, 'blog/home.html', {'posts': posts})
+
+
+
+
+
+
+
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Post
+from .forms import PostForm
+
+def update_post(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES, instance=post)
+        if form.is_valid():
+            form.save()
+            return redirect('post_detail', post_id=post.id)
+    else:
+        form = PostForm(instance=post)
+
+    return render(request, 'blog/update_post.html', {'form': form, 'post': post})
